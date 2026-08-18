f1f591a6ea9dd8b146331b9fc81f435f2e7744b6
Prajwal H G <prajuu187@gmail.com>
2026-08-18 12:59:24 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+abstract class Shape {
+    private String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getcolor() {
+        return color;
+    }
+
+    public abstract double calculateArea();
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
